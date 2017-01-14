a02e0bf49d84a81e65c348b135d5b4b5573393f2
Muneeb Khan <munibkhan7737@gmail.com>
2017-01-14 18:07:29 +0500

    UI is perfectly working

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var li_correct_cls = document.getElementsByTagName('li');
 var score_span = document.getElementById('score');
 var per_span = document.getElementById('per');
-var result_btn = document.getElementById('result-btn');
-var result_contain = document.getElementById('result');
-var get_result_contain = document.getElementById('get-result');
-var dialogue_box_corr = document.getElementById('dialogue-box-corr');
-var dialogue_box_incorr = document.getElementById('dialogue-box-incorr');
+var begin_contain = document.getElementById('begin');
+var start_btn = document.getElementById('start');
+var ques1_contain = document.getElementById('ques1');
 var score = 0;
 
 
+start_btn.addEventListener('click', function(){
+    begin_contain.className += ' hidden';
+    ques1_contain.className = "show";
+})
 for (var i = 0; i < li_correct_cls.length; i++) {
     li_correct_cls[i].addEventListener('click', function () {
         if (this.className == 'correct') {
@@ -19,19 +21,23 @@ for (var i = 0; i < li_correct_cls.length; i++) {
             score--;
             console.log(score);
         }
-        var show = this.parentElement.parentElement;
+        var show = this.parentElement.parentElement.parentElement.parentElement;
         show.className = 'hidden';
         show.nextElementSibling.className = 'show';
     }, false)
 }
 
-
 result_btn.onclick = function () {
-    get_result_contain.style.display = 'none';
-    result_contain.className = 'show';
-    var per = (score * 100) / 10;
-    score_span.innerHTML += " " + score;
-    per_span.innerHTML += " " + per + '%';
+    get_result_contain.className = 'hidden';
+    result_contain.className += ' show';
+    if (score > 1) {
+        var per = (score * 100) / 10;
+        score_span.innerHTML += " " + score;
+        per_span.innerHTML += " " + per + '%';
+    }
+    else {
+        score_span.innerHTML += " 0";
+    }
 }
 
 
