12423e54a1753c5a5531073e0324e97d9da4a563
Muneeb Khan <munibkhan7737@gmail.com>
2017-01-14 21:24:18 +0500

    app completed but I want to add some other features

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var per_span = document.getElementById('per');
 var begin_contain = document.getElementById('begin');
 var start_btn = document.getElementById('start');
 var ques1_contain = document.getElementById('ques1');
+var get_rslt_contain = document.getElementById('get-rslt')
+var get_rslt_btn = document.getElementById('get-rslt-btn');
+var result_contain = document.getElementById('result');
+
 var score = 0;
 
-
-start_btn.addEventListener('click', function(){
+start_btn.addEventListener('click', function () {
     begin_contain.className += ' hidden';
     ques1_contain.className = "show";
 })
@@ -15,11 +18,9 @@ for (var i = 0; i < li_correct_cls.length; i++) {
     li_correct_cls[i].addEventListener('click', function () {
         if (this.className == 'correct') {
             score++;
-            console.log(score);
         }
         else {
             score--;
-            console.log(score);
         }
         var show = this.parentElement.parentElement.parentElement.parentElement;
         show.className = 'hidden';
@@ -27,18 +28,31 @@ for (var i = 0; i < li_correct_cls.length; i++) {
     }, false)
 }
 
-result_btn.onclick = function () {
-    get_result_contain.className = 'hidden';
-    result_contain.className += ' show';
+get_rslt_btn.addEventListener('click', function () {
+    get_rslt_contain.className = 'hidden';
+    result_contain.className = 'show';
     if (score > 1) {
         var per = (score * 100) / 10;
-        score_span.innerHTML += " " + score;
-        per_span.innerHTML += " " + per + '%';
+        score_span.innerHTML = " " + score;
+        per_span.innerHTML = " " + per + '%';
     }
     else {
-        score_span.innerHTML += " 0";
+        score_span.innerHTML += "0";
+        per_span.innerHTML = "0 %" ;
     }
-}
+})
+// result_btn.onclick = function () {
+    // get_result_contain.className = 'hidden';
+    // result_contain.className += ' show';
+    // if (score > 1) {
+    //     var per = (score * 100) / 10;
+    //     score_span.innerHTML = " " + score;
+    //     per_span.innerHTML = " " + per + '%';
+    // }
+    // else {
+    //     score_span.innerHTML += " 0";
+    // }
+// }
 
 
 
